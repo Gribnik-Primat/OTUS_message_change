d4ccae0edac996dbd07d871e753df28f4905a78c
grigoriy.belyaev <grigoriy.belyaev@gs-labs.ru>
2023-09-23 20:13:07 +0300

    modifications

--- test_receive_message.py
+++ test_receive_message.py
@@ -2,11 +2,13 @@ import pytest
 import json
 from main import app
 
+
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+
 
 def test_receive_message(client):
     data = {
